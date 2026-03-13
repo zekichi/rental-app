c67770a6f4d1308418d68ff902c336cb24b943d3
Ezequiel <equintana779@gmail.com>
2026-03-13 13:59:35 -0300

    pagemain

--- app/(routes)/(dashboard)/dashboard/page.tsx
+++ app/(routes)/(dashboard)/dashboard/page.tsx
@@ -1,5 +1,10 @@
 export default function DashboardPage() {
-  return (
-    <div>DashboardPage</div>
+  return (<div>
+
+    <div className="flex justify-between">
+      <h2 className="text-2xl font-bold">List of Cars</h2>
+    </div>
+    <p>Listado de coches</p>
+    </div>
   )
 }
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+    return(
+        <div>
+            <p>Hello Ezequiel</p>
+            <Button>This is a button</Button>
+        </div>
+    );
+}
